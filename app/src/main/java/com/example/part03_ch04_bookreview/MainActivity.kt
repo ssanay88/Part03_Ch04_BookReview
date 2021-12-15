65f1477b919f131d52436f95505734a81f712041
Yangsuhyuck <pplloo748@gmail.com>
2021-12-15 20:16:22 +0900

    2021 - 12 - 15

---
+++ app/src/main/java/com/example/part03_ch04_bookreview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.part03_ch04_bookreview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
